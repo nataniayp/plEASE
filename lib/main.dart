80bff1b633864766a2bad11371cdee216995a405
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-05-18 14:21:34 +0800

    Bottom nav bar (#1)
    * created basic bottom nav bar
    
    * linked bottomnavbar to diff screens

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:please/screens/request.dart';
 import 'package:please/screens/respond.dart';
 import 'package:please/screens/profile.dart';
 
+
 void main() {
   runApp(MyApp());
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:please/screens/request.dart';
+import 'package:please/screens/respond.dart';
+import 'package:please/screens/profile.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -8,14 +11,56 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    Request(),
+    Respond(),
+    Profile(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal[900],
-      body: SafeArea(
-          child: Container()
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'icons/interview.png',
+              color: Colors.teal[900],
+              height: 23,
+            ),
+            label: "Respond",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'icons/reply.png',
+              color: Colors.teal[900],
+              height: 23,
+            ),
+            label: "Request",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'icons/user.png',
+              color: Colors.teal[900],
+              height: 23,
+            ),
+            label: "Profile",
+          ),
+        ],
       ),
     );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
 
