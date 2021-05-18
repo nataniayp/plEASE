27977728426ddfafd4117e41d952c1f46705bf9c
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-05-18 13:51:58 +0800

    created basic bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:please/screens/request.dart';
+import 'package:please/screens/respond.dart';
 import 'package:please/screens/profile.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -12,9 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Please',
       routes: {
-        '/': (context) => Profile(),
+        '/': (context) => Request(),
+        '/profile': (context) => Profile(),
         // '/request': (context) => Request(),
-        // '/respond': (context) => Respond(),
+        '/respond': (context) => Respond(),
       },
     );
   }
