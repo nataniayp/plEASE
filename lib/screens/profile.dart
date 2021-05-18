9026244fef38e31ab20139b9716007ed4131db57
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-05-18 20:57:47 +0800

    added skeleton of profile design (#2)

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -19,67 +19,67 @@ class _ProfileState extends State<Profile> {
                 colors: [Colors.teal[900], Colors.grey[600]]
             ),
           ),
-          // child: Padding(
-          //   padding: const EdgeInsets.only(
-          //       top: 200.0,
-          //       bottom: 150.0),
-          //   child: Column(
-          //       children: <Widget>[
-          //         // SizedBox(
-          //         //   height: 200.0,
-          //         // ),
-          //         CircleAvatar(
-          //           radius: 75.0,
-          //           backgroundColor: Colors.grey,
-          //         ),
-          //         Padding(
-          //           padding: const EdgeInsets.symmetric(
-          //               horizontal: 100.0,
-          //               vertical: 20.0),
-          //           child: Divider(
-          //             height: 20.0,
-          //             color: Colors.white,
-          //           ),
-          //         ),
-          //         TextButton(
-          //           onPressed: () {},
-          //           child: Text(
-          //               '3 requests',
-          //               style: TextStyle(
-          //                 color: Colors.white,
-          //                 letterSpacing: 1.5,
-          //               )
-          //           ),
-          //         ),
-          //         SizedBox(
-          //           height: 10.0,
-          //         ),
-          //         TextButton(
-          //           onPressed: () {},
-          //           child: Text(
-          //               '5 responses',
-          //               style: TextStyle(
-          //                 color: Colors.white,
-          //                 letterSpacing: 1.5,
-          //               )
-          //           ),
-          //         ),
-          //         SizedBox(
-          //           height: 130.0,
-          //         ),
-          //         TextButton(
-          //           onPressed: () {},
-          //           child: Text(
-          //               'LOGOUT',
-          //               style: TextStyle(
-          //                 color: Colors.grey[500],
-          //                 letterSpacing: 1.7,
-          //               )
-          //           ),
-          //         ),
-          //       ]
-          //   ),
-          // ),
+          child: Padding(
+            padding: const EdgeInsets.only(
+                top: 200.0,
+                bottom: 150.0),
+            child: Column(
+                children: <Widget>[
+                  // SizedBox(
+                  //   height: 200.0,
+                  // ),
+                  CircleAvatar(
+                    radius: 75.0,
+                    backgroundColor: Colors.grey,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 100.0,
+                        vertical: 20.0),
+                    child: Divider(
+                      height: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                        '3 requests',
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 1.5,
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                        '5 responses',
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 1.5,
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 130.0,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                        'LOGOUT',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                          letterSpacing: 1.7,
+                        )
+                    ),
+                  ),
+                ]
+            ),
+          ),
         ),
       ),
     );
