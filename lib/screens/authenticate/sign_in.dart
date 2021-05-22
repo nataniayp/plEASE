c20be42d76169e6203fe8b31f61e2c3a3e56762a
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-05-22 18:25:53 +0800

    added register screen

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:please/services/auth.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key key}) : super(key: key);
@@ -8,8 +9,83 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
+  // text field state
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(0.1 * size.width),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Sign In",
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text("REGISTER"),
+                ),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.all(0.1 * size.width),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 0.1 * size.width,
+                    ),
+                    SizedBox(
+                      height: 0.1 * size.width,
+                    ),
+                    TextFormField(
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(
+                      height: 0.05 * size.width,
+                    ),
+                    TextFormField(
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(
+                      height: 0.1 * size.width,
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        print(email);
+                        print(password);
+                      },
+                      child: Text(
+                          'SIGN IN',
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                            letterSpacing: 1.7,
+                          )
+                      ),
+                    ),
+                  ]
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -3,6 +3,8 @@ import 'package:please/screens/home.dart';
 import 'package:please/models/user.dart';
 import 'package:provider/provider.dart';
 import 'package:please/screens/authenticate/authenticate.dart';
+import 'package:please/screens/authenticate/sign_in.dart';
+import 'package:please/screens/authenticate/register.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key key}) : super(key: key);
@@ -14,7 +16,8 @@ class Wrapper extends StatelessWidget {
 
     // return either Home or Authenticate widget
     if (user == null) {
-      return Authenticate();
+      // return Authenticate();
+      return Register();
     } else {
       return Home();
     }
