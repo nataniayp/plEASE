ffa95a4882e70b245b2bddcaa8b16f74f8e96e8a
Kenneth <e0543512@u.nus.edu>
2021-05-23 15:11:42 +0800

    my requests navigation done my requests UI done passing data done integrating backend not done

--- lib/screens/my_requests.dart
+++ lib/screens/my_requests.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:please/components/customised_app_bar.dart';
 import 'package:please/components/screen_header.dart';
 import 'package:please/components/request_card.dart';
+import 'package:please/components/request_item.dart';
 
 class MyRequests extends StatefulWidget {
   const MyRequests({Key key}) : super(key: key);
@@ -12,6 +13,8 @@ class MyRequests extends StatefulWidget {
 
 class _MyRequestsState extends State<MyRequests> {
 
+  RequestItem selected;
+
   List<RequestCard> myList = [
     RequestCard(
       userName: "Natania",
@@ -20,60 +23,19 @@ class _MyRequestsState extends State<MyRequests> {
       quantity: 1,
       time: DateTime.now(),
     ),
-    RequestCard(
-      userName: "Natania",
-      category: "stationery",
-      itemName: "stapler",
-      quantity: 1,
-      time: DateTime.now(),
-    ),
-    RequestCard(
-      userName: "Natania",
-      category: "cleaning",
-      itemName: "vacuum cleaner",
-      quantity: 1,
-      time: DateTime.now(),
-    ),
-    RequestCard(
-      userName: "Natania",
-      category: "others",
-      itemName: "more sleep",
-      quantity: 100,
-      time: DateTime.now(),
-    ),
-    RequestCard(
-      userName: "Natania",
-      category: "food",
-      itemName: "aglio olio",
-      quantity: 1,
-      time: DateTime.now(),
-    ),
-    RequestCard(
-      userName: "Natania",
-      category: "stationery",
-      itemName: "stapler",
-      quantity: 1,
-      time: DateTime.now(),
-    ),
-    RequestCard(
-      userName: "Natania",
-      category: "cleaning",
-      itemName: "vacuum cleaner",
-      quantity: 1,
-      time: DateTime.now(),
-    ),
-    RequestCard(
-      userName: "Natania",
-      category: "others",
-      itemName: "more sleep",
-      quantity: 100,
-      time: DateTime.now(),
-    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    selected = ModalRoute.of(context).settings.arguments;
+    if (selected != null) myList.add(new RequestCard(
+      userName: selected.id, 
+      category: selected.category,
+      itemName: selected.itemName,
+      quantity: selected.quantity,
+      time: selected.date,
+    ));
 
     return Scaffold(
       backgroundColor: Colors.white,
