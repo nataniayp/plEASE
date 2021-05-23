1775ff79cff6498e401fa2ea29df388314b4658c
Kenneth <e0543512@u.nus.edu>
2021-05-23 11:37:22 +0800

    my requests navigation done my requests UI not done

--- lib/screens/my_requests.dart
+++ lib/screens/my_requests.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:please/components/customised_app_bar.dart';
+import 'package:please/components/screen_header.dart';
+import 'package:please/components/request_card.dart';
 
 class MyRequests extends StatefulWidget {
   const MyRequests({Key key}) : super(key: key);
@@ -9,13 +11,92 @@ class MyRequests extends StatefulWidget {
 }
 
 class _MyRequestsState extends State<MyRequests> {
+
+  List<RequestCard> myList = [
+    RequestCard(
+      userName: "Natania",
+      category: "food",
+      itemName: "aglio olio",
+      quantity: 1,
+      time: DateTime.now(),
+    ),
+    RequestCard(
+      userName: "Natania",
+      category: "stationery",
+      itemName: "stapler",
+      quantity: 1,
+      time: DateTime.now(),
+    ),
+    RequestCard(
+      userName: "Natania",
+      category: "cleaning",
+      itemName: "vacuum cleaner",
+      quantity: 1,
+      time: DateTime.now(),
+    ),
+    RequestCard(
+      userName: "Natania",
+      category: "others",
+      itemName: "more sleep",
+      quantity: 100,
+      time: DateTime.now(),
+    ),
+    RequestCard(
+      userName: "Natania",
+      category: "food",
+      itemName: "aglio olio",
+      quantity: 1,
+      time: DateTime.now(),
+    ),
+    RequestCard(
+      userName: "Natania",
+      category: "stationery",
+      itemName: "stapler",
+      quantity: 1,
+      time: DateTime.now(),
+    ),
+    RequestCard(
+      userName: "Natania",
+      category: "cleaning",
+      itemName: "vacuum cleaner",
+      quantity: 1,
+      time: DateTime.now(),
+    ),
+    RequestCard(
+      userName: "Natania",
+      category: "others",
+      itemName: "more sleep",
+      quantity: 100,
+      time: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: CustomisedAppBar(name: 'Natania', withBackArrow: true,),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              CustomisedAppBar(name: 'Natania', withBackArrow: true,),
+              ScreenHeader(name: 'My Requests', withSortBy: true,),
+              Expanded(
+                child: Scrollbar(
+                  child: ListView.builder(
+                      itemCount: myList.length,
+                      itemBuilder: (context, index) {
+                        return myList[index];
+                      }
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
