52b042f73a3ddd29d628b28ff46bda72298c715e
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-05-27 18:04:39 +0800

    deleted modal route and eg in my_requests.dart

--- lib/screens/my_requests.dart
+++ lib/screens/my_requests.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:please/components/customised_app_bar.dart';
 import 'package:please/components/screen_header.dart';
 import 'package:please/components/request_card.dart';
-import 'package:please/components/request_item.dart';
 import 'package:please/models/user_credentials.dart';
 import 'package:please/models/user_data.dart';
 import 'package:please/services/database.dart';
@@ -17,19 +16,6 @@ class MyRequests extends StatefulWidget {
 }
 
 class _MyRequestsState extends State<MyRequests> {
-
-  // RequestItem selected;
-  //
-  // List<RequestCard> myList = [
-  //   RequestCard(
-  //     userName: "Natania",
-  //     category: "food",
-  //     itemName: "aglio olio",
-  //     quantity: 1,
-  //     selectedDate: DateTime.now(),
-  //     selectedTime: TimeOfDay.now(),
-  //   ),
-  // ];
   RequestCard convertMapToRequestCard(Map<String, dynamic> map) {
     return RequestCard(
       userName: map['name'],
@@ -37,7 +23,10 @@ class _MyRequestsState extends State<MyRequests> {
       itemName: map['item'],
       quantity: map['quantity'],
       selectedDate: DateTime.parse(map['date']),
-      selectedTime: TimeOfDay(hour: int.parse(map['time'].split(":")[0]), minute: int.parse(map['time'].split(":")[1].substring(0,2))),
+      selectedTime: TimeOfDay(
+        hour: int.parse(map['time'].split(":")[0]),
+        minute: int.parse(map['time'].split(":")[1].substring(0,2))
+      ),
     );
   }
 
@@ -56,15 +45,6 @@ class _MyRequestsState extends State<MyRequests> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    // selected = ModalRoute.of(context).settings.arguments;
-    // if (selected != null) myList.add(new RequestCard(
-    //   userName: selected.name,
-    //   category: selected.category,
-    //   itemName: selected.itemName,
-    //   quantity: selected.quantity,
-    //   selectedDate: selected.date,
-    //   selectedTime: selected.time,
-    // ));
     final user = Provider.of<UserData>(context);
 
     return Scaffold(
