50a96517ce03f934f288bac5e16e5fb398076f35
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-05-27 15:51:01 +0800

    added resList field in UserCredentials and database

--- lib/models/user_credentials.dart
+++ lib/models/user_credentials.dart
@@ -2,12 +2,14 @@ class UserCredentials {
 
   String uid;
   String name;
-  List req;
+  List reqList;
+  List resList;
 
-  UserCredentials(String uid, String name, List req){
+  UserCredentials(String uid, String name, List reqList, List resList){
     this.uid = uid;
     this.name = name;
-    this.req = req;
+    this.reqList = reqList;
+    this.resList = resList;
   }
 
 }
