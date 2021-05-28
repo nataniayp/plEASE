7ebe33b555b22d11c21faa3311f44f62eed1a577
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-05-28 16:10:47 +0800

    added accepted, acceptedBy and acceptedByUid in request_card constructor; changed final fields to non-final to have empty constructor (which has been added); request_card returns container() if widget is empty

--- lib/components/request_card.dart
+++ lib/components/request_card.dart
@@ -13,15 +13,18 @@ class RequestCard extends StatefulWidget {
     @required this.quantity,
     @required this.selectedDate,
     @required this.selectedTime,
+    this.accepted,
+    this.acceptedBy,
+    this.acceptedByUid,
   }) : super(key: key);
 
-  final String uid;
-  final String userName;
-  final String category;
-  final String itemName;
-  final int quantity;
-  final DateTime selectedDate;
-  final TimeOfDay selectedTime;
+  String uid;
+  String userName;
+  String category;
+  String itemName;
+  int quantity;
+  DateTime selectedDate;
+  TimeOfDay selectedTime;
   bool accepted = false;
   String acceptedBy;
   String acceptedByUid;
@@ -30,6 +33,12 @@ class RequestCard extends StatefulWidget {
     this.accepted = true;
     this.acceptedBy = acceptedBy;
     this.acceptedByUid = acceptedByUid;
+  }
+
+  RequestCard.empty();
+
+  bool isEmpty() {
+    return this.uid == null;
   }
 
 
@@ -59,84 +68,89 @@ class _RequestCardState extends State<RequestCard> {
       return format.format(dt);
     }
 
-    return Container(
-      height: 0.105 * size.height,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, 4),
-            blurRadius: 4,
-            color: Colors.black.withOpacity(0.25),
-          ),
-        ]
-      ),
-      padding: EdgeInsets.only(
-        top: 0.02 * size.height,
-        bottom: 0.02 * size.height,
-      ),
-      margin: EdgeInsets.only(
-        left: 0.1 * size.width,
-        right: 0.1 * size.width,
-        bottom: 0.02 * size.height,
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: EdgeInsets.all(0.01 * size.height),
-              child: Image.asset(
-                'assets/icons/${widget.category}.png',
-                color: Colors.teal[900],
+    if (!widget.isEmpty()) {
+      return Container(
+        height: 0.105 * size.height,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 4),
+                blurRadius: 4,
+                color: Colors.black.withOpacity(0.25),
               ),
-            ),
-            // child: Icon(
-            //   Icons.fastfood,
-            // ),
-          ),
-          Expanded(
-            flex: 6,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    "${widget.userName} wants"
-                  ),
+            ]
+        ),
+        padding: EdgeInsets.only(
+          top: 0.02 * size.height,
+          bottom: 0.02 * size.height,
+        ),
+        margin: EdgeInsets.only(
+          left: 0.1 * size.width,
+          right: 0.1 * size.width,
+          bottom: 0.02 * size.height,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.all(0.01 * size.height),
+                child: Image.asset(
+                  'assets/icons/${widget.category}.png',
+                  color: Colors.teal[900],
                 ),
-                Expanded(
-                  child: Text(
-                    "${widget.itemName} (${widget.quantity})",
-                    style: TextStyle(
-                      color: Colors.teal[900],
+              ),
+              // child: Icon(
+              //   Icons.fastfood,
+              // ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                        "${widget.userName} wants"
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Text(
-                    "by ${DateFormat('EEE, d/M/y,').format(widget.selectedDate)} ${widget.selectedTime.format(context)}"
+                  Expanded(
+                    child: Text(
+                      "${widget.itemName} (${widget.quantity})",
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: FlatButton(
-              onPressed: () async {
-                await Navigator.pushNamed(context, '/respond_details', arguments: rc);
-              },
-              child: Icon(
-                Icons.navigate_next,
+                  Expanded(
+                    child: Text(
+                        "by ${DateFormat('EEE, d/M/y,').format(widget.selectedDate)} ${widget.selectedTime.format(context)}"
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
-      ),
-    );
+            Expanded(
+              flex: 1,
+              child: FlatButton(
+                onPressed: () async {
+                  await Navigator.pushNamed(context, '/respond_details', arguments: rc);
+                },
+                child: Icon(
+                  Icons.navigate_next,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Container();
+    }
+
   }
 }
