c03c4b33eb3de8f0581266cef950203767412d41
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-05-29 12:55:09 +0800

    fixed layout

--- lib/screens/request.dart
+++ lib/screens/request.dart
@@ -30,185 +30,178 @@ class _RequestState extends State<Request> {
           children: <Widget>[
             CustomisedAppBar(),
             ScreenHeader(name: 'Request'),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0.1 * size.width,0,0.1 * size.width,0),
-              child: Column(
-                children: [
-                  Row (
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container( // Food
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 4,
-                              color: Colors.grey.withOpacity(0.4),
-                              offset: Offset(0, 8), // changes position of shadow
-                            ),
-                          ]
-                        ),
-                        height: size.height * 0.22,
-                        width: size.height * 0.22,
-                        child: FlatButton(
-                          onPressed: () {
-                            selected.category = 'Food';
-                            Navigator.pushNamed(context, '/request_input', arguments: selected);
-                          },
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(0, size.height * 0.05, 0, 0),
-                            child: Column(
-                              children: [
-                                Image.asset(
-                                  'assets/icons/food.png',
-                                  color: Colors.teal[900],
-                                  scale: 8,
-                                ),
-                                SizedBox(height: size.height * 0.03),
-                                Text(
-                                  'Food',
-                                  style: TextStyle(
-                                    color: Colors.teal[900],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
+            Row (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container( // Food
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 4,
+                        color: Colors.grey.withOpacity(0.4),
+                        offset: Offset(0, 8), // changes position of shadow
                       ),
-                      SizedBox(width: size.width * 0.04),
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                blurRadius: 4,
-                                color: Colors.grey.withOpacity(0.4),
-                                offset: Offset(0, 8), // changes position of shadow
-                              ),
-                            ]
-                        ),
-                        height: size.height * 0.22,
-                        width: size.height * 0.22,
-                        child: FlatButton(
-                          onPressed: () {
-                            selected.category = 'Stationery';
-                            Navigator.pushNamed(context, '/request_input', arguments: selected);
-                          },
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(0, size.height * 0.05, 0, 0),
-                            child: Column(
-                              children: [
-                                Image.asset(
-                                  'assets/icons/stationery.png',
-                                  color: Colors.teal[900],
-                                  scale: 8,
-                                ),
-                                SizedBox(height: size.height * 0.03),
-                                Text(
-                                  'Stationery',
-                                  style: TextStyle(
-                                    color: Colors.teal[900],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ]
                   ),
-                  SizedBox(height: size.width * 0.02),
-                  Row (
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                blurRadius: 4,
-                                color: Colors.grey.withOpacity(0.4),
-                                offset: Offset(0, 8), // changes position of shadow
-                              ),
-                            ]
-                        ),
-                        height: size.height * 0.22,
-                        width: size.height * 0.22,
-                        child: FlatButton(
-                          onPressed: () {
-                            selected.category = 'Cleaning supplies';
-                            Navigator.pushNamed(context, '/request_input', arguments: selected);
-                          },
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(0, size.height * 0.05, 0, 0),
-                            child: Column(
-                              children: [
-                                Image.asset(
-                                  'assets/icons/cleaning.png',
-                                  color: Colors.teal[900],
-                                  scale: 8,
-                                ),
-                                SizedBox(height: size.height * 0.03),
-                                Text(
-                                  'Cleaning supplies',
-                                  style: TextStyle(
-                                    color: Colors.teal[900],
-                                  ),
-                                ),
-                              ],
+                  height: size.height * 0.22,
+                  width: size.height * 0.22,
+                  child: FlatButton(
+                    onPressed: () {
+                      selected.category = 'Food';
+                      Navigator.pushNamed(context, '/request_input', arguments: selected);
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, size.height * 0.05, 0, 0),
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'assets/icons/food.png',
+                            color: Colors.teal[900],
+                            scale: 8,
+                          ),
+                          SizedBox(height: size.height * 0.03),
+                          Text(
+                            'Food',
+                            style: TextStyle(
+                              color: Colors.teal[900],
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                      SizedBox(width: size.width * 0.04),
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                blurRadius: 4,
-                                color: Colors.grey.withOpacity(0.4),
-                                offset: Offset(0, 8), // changes position of shadow
-                              ),
-                            ]
-                        ),
-                        height: size.height * 0.22,
-                        width: size.height * 0.22,
-                        child: FlatButton(
-                          onPressed: () {
-                            selected.category = 'Others';
-                            Navigator.pushNamed(context, '/request_input', arguments: selected);
-                          },
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(0, size.height * 0.05, 0, 0),
-                            child: Column(
-                              children: [
-                                Image.asset(
-                                  'assets/icons/audio-jack.png',
-                                  color: Colors.teal[900],
-                                  scale: 8,
-                                ),
-                                SizedBox(height: size.height * 0.03),
-                                Text(
-                                  'Others',
-                                  style: TextStyle(
-                                    color: Colors.teal[900],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ],
-              ),
+                ),
+                SizedBox(width: size.width * 0.04),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 4,
+                          color: Colors.grey.withOpacity(0.4),
+                          offset: Offset(0, 8), // changes position of shadow
+                        ),
+                      ]
+                  ),
+                  height: size.height * 0.22,
+                  width: size.height * 0.22,
+                  child: FlatButton(
+                    onPressed: () {
+                      selected.category = 'Stationery';
+                      Navigator.pushNamed(context, '/request_input', arguments: selected);
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, size.height * 0.05, 0, 0),
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'assets/icons/stationery.png',
+                            color: Colors.teal[900],
+                            scale: 8,
+                          ),
+                          SizedBox(height: size.height * 0.03),
+                          Text(
+                            'Stationery',
+                            style: TextStyle(
+                              color: Colors.teal[900],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: size.width * 0.02),
+            Row (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 4,
+                          color: Colors.grey.withOpacity(0.4),
+                          offset: Offset(0, 8), // changes position of shadow
+                        ),
+                      ]
+                  ),
+                  height: size.height * 0.22,
+                  width: size.height * 0.22,
+                  child: FlatButton(
+                    onPressed: () {
+                      selected.category = 'Cleaning supplies';
+                      Navigator.pushNamed(context, '/request_input', arguments: selected);
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, size.height * 0.05, 0, 0),
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'assets/icons/cleaning.png',
+                            color: Colors.teal[900],
+                            scale: 8,
+                          ),
+                          SizedBox(height: size.height * 0.03),
+                          Text(
+                            'Cleaning supplies',
+                            style: TextStyle(
+                              color: Colors.teal[900],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: size.width * 0.04),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 4,
+                          color: Colors.grey.withOpacity(0.4),
+                          offset: Offset(0, 8), // changes position of shadow
+                        ),
+                      ]
+                  ),
+                  height: size.height * 0.22,
+                  width: size.height * 0.22,
+                  child: FlatButton(
+                    onPressed: () {
+                      selected.category = 'Others';
+                      Navigator.pushNamed(context, '/request_input', arguments: selected);
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, size.height * 0.05, 0, 0),
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'assets/icons/audio-jack.png',
+                            color: Colors.teal[900],
+                            scale: 8,
+                          ),
+                          SizedBox(height: size.height * 0.03),
+                          Text(
+                            'Others',
+                            style: TextStyle(
+                              color: Colors.teal[900],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
