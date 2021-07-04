1d9a23e9f57d5fb861ec88048e8e3c5f86f8e74b
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-07-04 17:21:32 +0800

    notification setup in profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:please/services/auth.dart';
+import 'package:http/http.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key key}) : super(key: key);
@@ -12,6 +15,35 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   final AuthService _auth = AuthService();
+
+  Future<Response> sendNotification(List<String> tokenIdList, String contents, String heading) async{
+
+    return await post(
+      Uri.parse('https://onesignal.com/api/v1/notifications'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>
+      {
+        "app_id": '729732d6-6359-40bb-8847-ca6e0c987d25',//kAppId is the App Id that one get from the OneSignal When the application is registered.
+
+        "include_player_ids": tokenIdList,//tokenIdList Is the List of All the Token Id to to Whom notification must be sent.
+
+        // android_accent_color reprsent the color of the heading text in the notifiction
+        "android_accent_color":"FF9976D2",
+
+        "small_icon":"ic_stat_onesignal_default",
+
+        "large_icon":"https://www.filepicker.io/api/file/zPloHSmnQsix82nlj9Aj?filename=name.jpg",
+
+        "headings": {"en": heading},
+
+        "contents": {"en": contents},
+
+
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -75,6 +107,12 @@ class _ProfileState extends State<Profile> {
                         letterSpacing: 1.5,
                       )
                   ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    sendNotification(['31ea5c49-3c14-4b2f-a93e-9a592e901b9b'], 'Your request has been accepted!', 'plEASE');
+                  },
+                  child: Text('Send notification'),
                 ),
                 SizedBox(
                   height: 0.1 * size.height,
