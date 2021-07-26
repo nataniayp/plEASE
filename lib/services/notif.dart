3fd5b8adfe3f41607086d438afdff14adda3d68d
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-07-26 17:00:48 +0800

    added feed ranking sorting

--- lib/services/notif.dart
+++ lib/services/notif.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart';
 
 class Notif {
 
-  Future<Response> sendNotification(List<String> tokenIdList, String contents, String heading) async {
+  Future<Response> sendNotification(List<String> tokenIdList) async {
     return await post(
       Uri.parse('https://onesignal.com/api/v1/notifications'),
       headers: <String, String>{
@@ -12,20 +12,13 @@ class Notif {
       },
       body: jsonEncode(<String, dynamic>
       {
-        "app_id": '729732d6-6359-40bb-8847-ca6e0c987d25',//kAppId is the App Id that one get from the OneSignal When the application is registered.
-
-        "include_player_ids": tokenIdList,//tokenIdList Is the List of All the Token Id to to Whom notification must be sent.
-
-        // android_accent_color represent the color of the heading text in the notification
+        "app_id": '729732d6-6359-40bb-8847-ca6e0c987d25',
+        "include_player_ids": tokenIdList,
         "android_accent_color":"FF9976D2",
-
         "small_icon":"https://i.ibb.co/S5FHpXF/pl-EASE-1028.png",
-
         "large_icon":"https://i.ibb.co/S5FHpXF/pl-EASE-1028.png",
-
-        "headings": {"en": heading},
-
-        "contents": {"en": contents},
+        "headings": {"en": 'plEASE'},
+        "contents": {"en": 'Your request has been accepted!'},
       }),
     );
   }
